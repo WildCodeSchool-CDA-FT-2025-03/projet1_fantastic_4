2c30ed437733dee752137fba253cae849d0956f4
Maximilien <maximilien.philippe@protonmail.com>
2025-03-27 19:16:47 +0100

    chore(000-ft): config router
    add router.tsx with minimal route page, add outlet in app and provider

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
+import { Outlet } from "react-router";
 import "./App.css";
 
 function App() {
   return (
     <>
       <h1>Projet 1, Les 4 fantastiques</h1>
+      <Outlet></Outlet>
     </>
   );
 }
