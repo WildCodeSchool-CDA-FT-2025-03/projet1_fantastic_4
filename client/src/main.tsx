3b0acf599376550cef8714bed3cb4878b2027ead
Maximilien <maximilien.philippe@protonmail.com>
2025-03-28 11:00:24 +0100

    fix(000-ft): correct import path

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,7 +4,7 @@ import { RouterProvider } from "react-router/dom";
 import { ApolloProvider } from "@apollo/client";
 import router from "./routers/routers.tsx";
 import "./global.css";
-import clientApollo from "./services/client-apollo.services.ts";
+import clientApollo from "./services/client-apollo.service.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
