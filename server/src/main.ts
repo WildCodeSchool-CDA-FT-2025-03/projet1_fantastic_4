7a8aff5660d23d7be9c51ec767f61f16d279c3f3
Maximilien <maximilien.philippe@protonmail.com>
2025-03-28 14:57:36 +0100

    chore(000-ft): add config .env.development
    add file .env.development, add pkg env-cmd, change script dev cmd

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,8 @@ import { startStandaloneServer } from "@apollo/server/standalone";
 import { getMedias } from "./resolvers/media.resolver";
 import logger from "./services/logger.service";
 
+import "dotenv";
+
 const typeDefs = `#graphql
   type Query {
     getMedias: [String],
@@ -21,9 +23,11 @@ const server = new ApolloServer({
   resolvers,
 });
 
+const PORT = process.env.APOLLO_SERVER_PORT || "4000";
+
 (async () => {
   const { url } = await startStandaloneServer(server, {
-    listen: { port: 4000 },
+    listen: { port: parseInt(PORT) },
   });
 
   logger.info(`🚀  Server ready at: ${url}`);
