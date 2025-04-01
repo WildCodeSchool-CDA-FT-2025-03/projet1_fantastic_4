39e01d10e0ba89a01d32012e98c7f37a866b4361
Maximilien <maximilien.philippe@protonmail.com>
2025-04-01 14:27:35 +0200

    fix(000-ft): delete input, use entity

--- server/src/inputs/media.input.ts
+++
@@ -1,9 +0,0 @@
-import { ObjectType } from "type-graphql";
-import { InputType, Field } from "type-graphql";
-
-@ObjectType()
-@InputType()
-export class MediaInput {
-  @Field()
-  name: string;
-}
