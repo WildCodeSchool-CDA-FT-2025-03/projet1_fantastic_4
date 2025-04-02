2b2afbb636d76a5c81be6790f2bfb717034e5a3e
Emilie <150048527+EmiLy-Ly-san@users.noreply.github.com>
2025-04-02 16:48:33 +0200

    feat(005): global.css integrated on App.tsx + set up globals balises in global.css

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router";
 import "./App.css";
+import "@/global.css";
 
 function App() {
   return (
     <>
-      <h1>Projet 1, Les 4 fantastiques</h1>
-      <Outlet></Outlet>
+      <main>
+        <h1>Projet 1, Les 4 fantastiques</h1>
+        <Outlet></Outlet>
+      </main>
     </>
   );
 }
