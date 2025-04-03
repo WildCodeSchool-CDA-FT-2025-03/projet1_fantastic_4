210f481d2cd7235cdaeb56b9259f41fde5200465
abdifatah <abdifatah4@msn.com>
2025-04-03 10:41:43 +0200

    feat(046-us): user resolver removed

--- server/src/main.ts
+++ server/src/main.ts
@@ -8,7 +8,6 @@ import dataSource from "./services/datas.service";
 
 import "dotenv";
 import CategoriesResolver from "./resolvers/categories.resolver";
-import { UsersResolver } from "./resolvers/users.resolver";
 
 const PORT = process.env.APOLLO_SERVER_PORT || "4000";
 
@@ -16,7 +15,7 @@ const PORT = process.env.APOLLO_SERVER_PORT || "4000";
   await dataSource.initialize();
 
   const schema = await buildSchema({
-    resolvers: [MediaResolver, CategoriesResolver, UsersResolver],
+    resolvers: [MediaResolver, CategoriesResolver],
   });
 
   const server = new ApolloServer({
