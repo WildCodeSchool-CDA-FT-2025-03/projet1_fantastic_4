4b2f6a6f258969ddfcc2feaad75e87d21ae9ce46
Emilie <150048527+EmiLy-Ly-san@users.noreply.github.com>
2025-04-03 18:05:11 +0200

    fix(005): import new json file for fakeData

--- client/src/pages/home/HomePage.tsx
+++ client/src/pages/home/HomePage.tsx
@@ -1,18 +1,6 @@
 import "@/pages/home/HomePage.css";
 import Carousel from "@/components/Carousel/Carousel";
-
-const fakeDatas = [
-  { id: 1, title: "Title 1", category_name: "games", genre: "Horror" },
-  { id: 2, title: "Title 2", category_name: "musics", genre: "Punk" },
-  { id: 3, title: "Title 3", category_name: "books", genre: "Drama" },
-  { id: 4, title: "Title 4", category_name: "movies", genre: "Fiction" },
-  { id: 5, title: "Title 5", category_name: "games", genre: "Adventure" },
-  { id: 6, title: "Title 6", category_name: "movies", genre: "Comedy" },
-  { id: 7, title: "Title 7", category_name: "books", genre: "Fiction" },
-  { id: 8, title: "Title 8", category_name: "musics", genre: "Rock" },
-  { id: 9, title: "Title 9", category_name: "games", genre: "Retro" },
-  { id: 10, title: "Title 10", category_name: "movies", genre: "Horror" },
-];
+import fakeDatasCarousel from "@/utiles/fakeDatasCarousel.json";
 
 const HomePage = () => {
   return (
@@ -22,8 +10,10 @@ const HomePage = () => {
           Welcome to your <span>Home Center</span> !
         </h1>
         <section>
-          <h2 className="home-page-subtitle">Recommandations</h2>
-          <Carousel datas={fakeDatas} />
+          <Carousel
+            datas={fakeDatasCarousel}
+            title_carousel="Recommandations"
+          />
         </section>
       </div>
     </>
