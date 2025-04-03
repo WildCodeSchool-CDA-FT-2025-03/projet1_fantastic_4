ed3761ec3512513eaffaca6df943da23b828a899
Maximilien <maximilien.philippe@protonmail.com>
2025-04-03 19:21:30 +0200

    feat(020-us): add star icon

---
+++ client/src/components/Icons/Stars.tsx
@@ -0,0 +1,15 @@
+const StarIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    width="24"
+    height="24"
+    fill="gold"
+    stroke="black"
+    strokeWidth="0.5"
+  >
+    <path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.886 1.436 8.291L12 18.896l-7.372 4.587 1.436-8.291L.001 9.306l8.332-1.151z" />
+  </svg>
+);
+
+export default StarIcon;
