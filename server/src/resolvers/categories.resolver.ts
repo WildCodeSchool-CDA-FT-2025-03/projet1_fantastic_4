9697fb130b40895d709e5356473d94b504bad822
abdifatah <abdifatah4@msn.com>
2025-04-03 10:14:17 +0200

    feat(045-us): getCategoryById removed

--- server/src/resolvers/categories.resolver.ts
+++ server/src/resolvers/categories.resolver.ts
@@ -1,4 +1,4 @@
-import { Resolver, Query, Arg } from "type-graphql";
+import { Resolver, Query } from "type-graphql";
 import { CategoriesEntity } from "@/entities/categories.entity";
 
 @Resolver()
@@ -6,15 +6,6 @@ class CategoriesResolver {
   @Query(() => [CategoriesEntity])
   async getAllCategories() {
     return await CategoriesEntity.find();
-  }
-
-  @Query(() => CategoriesEntity)
-  async getCategoriesById(@Arg("id") id: number): Promise<CategoriesEntity> {
-    const category = await CategoriesEntity.findOneBy({ id: id });
-    if (!category) {
-      throw new Error("Category not found");
-    }
-    return category;
   }
 }
 
