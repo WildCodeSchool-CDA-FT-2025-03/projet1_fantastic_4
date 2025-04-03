5ab9049fbb8b3c3d81963f993501b8c535314a51
abdifatah <abdifatah4@msn.com>
2025-04-03 16:54:50 +0200

    feat(047-task): migrate categories done

---
+++ server/src/services/migration/migrate.ts
@@ -0,0 +1,30 @@
+import dataSource from "../datas.service";
+import { log } from "console";
+import { default as categoriesData } from "./categories.json";
+import { CategoriesEntity } from "../../entities/categories.entity";
+
+(async () => {
+  await dataSource.initialize();
+  const queryRunner = dataSource.createQueryRunner();
+
+  try {
+    await queryRunner.startTransaction();
+    await queryRunner.query("DELETE FROM categories");
+    await queryRunner.query("DELETE FROM sqlite_sequence");
+
+    const categories = categoriesData;
+
+    const newCategories = categories.map((category) => {
+      const newCategory = new CategoriesEntity();
+      newCategory.name = category.name;
+      return newCategory;
+    });
+
+    const res = await dataSource.manager.save(newCategories);
+    log(res);
+    await queryRunner.commitTransaction();
+  } catch (error) {
+    log(error);
+    await queryRunner.rollbackTransaction();
+  }
+})();
