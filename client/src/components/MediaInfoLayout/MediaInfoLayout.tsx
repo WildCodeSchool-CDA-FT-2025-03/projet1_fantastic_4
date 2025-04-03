ba21cd48fb886998d34a24739ec0f633204689b4
Maximilien <maximilien.philippe@protonmail.com>
2025-04-03 14:31:39 +0200

    feat(020-us): add dynamique info

--- client/src/components/MediaInfoLayout/MediaInfoLayout.tsx
+++ client/src/components/MediaInfoLayout/MediaInfoLayout.tsx
@@ -1,29 +1,50 @@
 import "./MediaInfoLayout.css";
 
-const MediaInfoLayout = () => {
+type PropsMediaInfoLayout = {
+  genre: "game" | "music" | "movie" | "book";
+  title: string;
+  releaseDate: string;
+  summary: string;
+  rating: number;
+  isFavorite: boolean;
+  subtitle?: string;
+  secondaryInfo?: { title: string; text: string }[];
+};
+
+const MediaInfoLayout = (props: PropsMediaInfoLayout) => {
+  const title = props.title;
+  const _releaseDate = props.releaseDate;
+  const summary = props.summary;
+  const _rating = props.rating;
+  const _isFavorite = props.isFavorite;
+  const subtitle = props.subtitle;
+  const secondaryInfo = props.secondaryInfo;
+
   return (
     <div className="media-info-layout">
       <div className="media-info-layout-top">
-        <div className="title">Factorio</div>
+        <div className="title">{title}</div>
         <div className="raiting">X X X X X</div>
       </div>
       <div className="media-info-layout-main">
         <div className="media-info-layout-main-left">
           <div className="cover"></div>
-          <div className="subtitle">Une super legende</div>
+          <div className="subtitle">{subtitle}</div>
         </div>
-        <div className="media-info-layout-main-description">
-          Un jeux video trop bien, tu dois l'acheter!! et oui!!'
-        </div>
+        <div className="media-info-layout-main-description">{summary}</div>
       </div>
       <div className="media-info-layout-others">
-        <div className="media-info-layout-other-item">
-          <div className="item-title">
-            <span>Release date </span>
-            <hr />
-          </div>
-          <p className="item-info"> 15-11-2025</p>
-        </div>
+        {secondaryInfo?.map((info) => {
+          return (
+            <div className="media-info-layout-other-item">
+              <div className="item-title">
+                <span>{info.title}</span>
+                <hr />
+              </div>
+              <p className="item-info">{info.text}</p>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
--- client/src/pages/games/GameInfo/GameInfo.tsx
+++ client/src/pages/games/GameInfo/GameInfo.tsx
@@ -4,9 +4,39 @@ import "./GameInfo.css";
 
 const GameInfo = () => {
   const { _id } = useParams();
+
+  const game = {
+    title: "Factorio",
+    releseDate: "21-08-2025",
+    summary:
+      "Factorio a pour but la création d'une usine complètement automatisée, produisant des éléments de plus en plus complexes, dans un monde en 2D. Utiliser votre imagination pour concevoir votre usine, combiner de simples éléments en ingénieuses structures, tout en vous protégeant des aliens !",
+    subtitle: "Best game ever!",
+    gameTags: ["horror", "action", "RPG", "Open "],
+    rating: 3.6,
+    isFavorite: true,
+    pegi: "PEGI 16",
+    developer: "Maximilien Compamy",
+    publisher: "Valve",
+  };
   return (
     <>
-      <MediaInfoLayout />
+      <MediaInfoLayout
+        genre="game"
+        title={game.title}
+        releaseDate={game.releseDate}
+        summary={game.summary}
+        rating={game.rating}
+        isFavorite={game.isFavorite}
+        subtitle={game.subtitle}
+        secondaryInfo={[
+          { title: "Developer", text: game.developer },
+          {
+            title: "Publisher",
+            text: game.publisher,
+          },
+          { title: "Pegi", text: game.pegi },
+        ]}
+      />
     </>
   );
 };
