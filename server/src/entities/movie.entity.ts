c830ba06495522d4fb8a9efdcba521cb6edf07e9
Emilie <150048527+EmiLy-Ly-san@users.noreply.github.com>
2025-04-04 11:33:25 +0200

    feat(043): work in progress entity Movie

---
+++ server/src/entities/movie.entity.ts
@@ -0,0 +1,64 @@
+import { Field, ObjectType } from "type-graphql";
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@ObjectType()
+@Entity()
+export class MovieEntity extends BaseEntity {
+  @Field()
+  @PrimaryGeneratedColumn("increment")
+  id: number;
+
+  @Column()
+  @Field()
+  title: string;
+
+  @Column("text", { array: true })
+  @Field(() => [String])
+  producers: string[];
+  //TODO Relation with movie_producer table
+
+  @Column("text", { array: true })
+  @Field(() => [String])
+  studios: string[];
+
+  @Column()
+  @Field()
+  releaseDate: string;
+
+  @Column()
+  @Field()
+  duration: number;
+
+  @Column("text", { array: true })
+  @Field(() => [String])
+  genre: string[];
+  //TODO Relation with movie_producer table
+
+  @Column()
+  @Field()
+  summary: string;
+
+  @Column()
+  @Field()
+  targetedAudience: string;
+
+  @Column("text", { array: true })
+  @Field(() => [String])
+  awards: string[];
+
+  @Column("text", { array: true })
+  @Field(() => [String])
+  actors: string;
+
+  @Column()
+  @Field()
+  budget: number;
+
+  @Column()
+  @Field()
+  boxOffice: number;
+
+  @Column()
+  @Field()
+  rate: number;
+}
