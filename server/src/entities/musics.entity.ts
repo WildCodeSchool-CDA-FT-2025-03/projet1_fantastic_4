3b0c6d9bacbdf22d1b7d6db52d0e1b7fa69925aa
abdifatah <abdifatah4@msn.com>
2025-04-04 12:09:59 +0200

    feat(04-feat): entity and resolver created

---
+++ server/src/entities/musics.entity.ts
@@ -0,0 +1,39 @@
+import { Field, ObjectType } from "type-graphql";
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@ObjectType()
+@Entity("musics")
+export class MusicsEntity extends BaseEntity {
+  @Column()
+  @Field()
+  @PrimaryGeneratedColumn("increment")
+  id: number;
+
+  @Column()
+  @Field()
+  title: string;
+
+  @Column()
+  @Field()
+  artists: string;
+
+  @Column()
+  @Field()
+  label: string;
+
+  @Column()
+  @Field()
+  duration: string;
+
+  @Column()
+  @Field()
+  releaseDate: Date;
+
+  @Column()
+  @Field()
+  isbn: string;
+
+  @Column()
+  @Field()
+  series: boolean;
+}
