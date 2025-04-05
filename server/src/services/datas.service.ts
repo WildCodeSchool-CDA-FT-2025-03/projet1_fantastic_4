02768bf6ba21d8463f8efb8f4afddcd1929e72da
Maximilien <maximilien.philippe@protonmail.com>
2025-04-05 18:04:09 +0200

    feat(020-us): insert languages entity to data service

--- server/src/services/datas.service.ts
+++ server/src/services/datas.service.ts
@@ -2,11 +2,12 @@ import { GamesEntity } from "../entities/games/games.entity";
 import { CategoriesEntity } from "../entities/categories.entity";
 import { DataSource } from "typeorm";
 import { GamesPegiEsbr } from "../entities/games/pegiesbr.entity";
+import { GamesLanguages } from "../entities/games/languages.entity";
 
 const dataSource = new DataSource({
   type: "sqlite",
   database: "./db.sqlite",
-  entities: [CategoriesEntity, GamesEntity, GamesPegiEsbr],
+  entities: [CategoriesEntity, GamesEntity, GamesPegiEsbr, GamesLanguages],
   synchronize: true, //pas en prod
 });
 
