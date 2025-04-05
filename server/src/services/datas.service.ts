43f481c5fe6da9dd71cc7e72dffd4553ab23832e
Florian Sebal <fsebal91@gmail.com>
2025-04-05 15:44:44 +0200

    feat(042-ft): add all books entities to the dataservices

--- server/src/services/datas.service.ts
+++ server/src/services/datas.service.ts
@@ -1,10 +1,24 @@
 import { CategoriesEntity } from "@/entities/categories.entity";
 import { DataSource } from "typeorm";
+import { BooksEntity } from "@/entities/books.entity";
+import { BookFavoritesEntity } from "@/entities/book_favorites.entity";
+import { BookRatingEntity } from "@/entities/book_rating.entity";
+import { GenresEntity } from "@/entities/genres.entity";
+import { PublishersEntity } from "@/entities/publishers.entity";
+import { TitlesEntity } from "@/entities/titles.entity";
 
 const dataSource = new DataSource({
   type: "sqlite",
   database: "./db.sqlite",
-  entities: [CategoriesEntity],
+  entities: [
+    CategoriesEntity,
+    BooksEntity,
+    BookFavoritesEntity,
+    BookRatingEntity,
+    GenresEntity,
+    PublishersEntity,
+    TitlesEntity,
+  ],
   synchronize: true, //pas en prod
 });
 
