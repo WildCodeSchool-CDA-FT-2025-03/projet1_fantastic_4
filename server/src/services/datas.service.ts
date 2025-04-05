0a7ee6c765dbf05829bd10811bfd9b6fc7918643
Maximilien <maximilien.philippe@protonmail.com>
2025-04-05 12:48:03 +0200

    feat(020-us): add games entity to datas service

--- server/src/services/datas.service.ts
+++ server/src/services/datas.service.ts
@@ -1,10 +1,11 @@
+import { GamesEntity } from "../entities/games/games.entity";
 import { CategoriesEntity } from "../entities/categories.entity";
 import { DataSource } from "typeorm";
 
 const dataSource = new DataSource({
   type: "sqlite",
   database: "./db.sqlite",
-  entities: [CategoriesEntity],
+  entities: [CategoriesEntity, GamesEntity],
   synchronize: true, //pas en prod
 });
 
