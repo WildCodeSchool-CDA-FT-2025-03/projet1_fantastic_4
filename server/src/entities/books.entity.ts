3c656efd8c664d571e5f94d93009318145be273e
Florian Sebal <fsebal91@gmail.com>
2025-04-05 14:23:58 +0200

    feat(042-ft): add books entity main structure work in progress

---
+++ server/src/entities/books.entity.ts
@@ -0,0 +1,46 @@
+import { Field, ObjectType } from "type-graphql";
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@ObjectType()
+@Entity()
+export class BooksEntity extends BaseEntity {
+  @Field()
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Field()
+  @Column()
+  author: string;
+
+  @Field()
+  @Column()
+  publisher: string;
+
+  @Field()
+  @Column()
+  genre: string;
+
+  @Field()
+  @Column()
+  synopsis: string;
+
+  @Field()
+  @Column()
+  audiencerate: string;
+
+  @Field()
+  @Column()
+  booktitle: string;
+
+  @Field()
+  @Column()
+  series: string;
+
+  @Field()
+  @Column()
+  releasedate: Date;
+
+  @Field()
+  @Column()
+  categoryname: string;
+}
