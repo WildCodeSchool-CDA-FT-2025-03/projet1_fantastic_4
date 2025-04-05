efa86947dbf08b6e606aff7030a3af4c3ccfa11e
Maximilien <maximilien.philippe@protonmail.com>
2025-04-05 20:22:58 +0200

    feat(020-us): add companies entity

---
+++ server/src/entities/games/companies.entity.ts
@@ -0,0 +1,29 @@
+import { Field, ObjectType } from "type-graphql";
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+  Unique,
+} from "typeorm";
+import { GamesEntity } from "./games.entity";
+
+@ObjectType()
+@Entity("companies")
+@Unique(["name"])
+export class CompaniesEntity extends BaseEntity {
+  @Field()
+  @PrimaryGeneratedColumn("uuid")
+  id: number;
+
+  @Field()
+  @Column()
+  name: string;
+
+  @ManyToMany(() => GamesEntity, (game) => game.developers)
+  gamesDevelopers: GamesEntity[];
+
+  @ManyToMany(() => GamesEntity, (game) => game.publishers)
+  gamesPublishers: GamesEntity[];
+}
