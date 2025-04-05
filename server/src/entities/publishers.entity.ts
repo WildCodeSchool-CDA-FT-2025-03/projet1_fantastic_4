25b431d9d26e1e1eea6517a77f40d19b20945b80
Florian Sebal <fsebal91@gmail.com>
2025-04-05 15:15:26 +0200

    feat(042-ft): correction nom de l'entité

--- server/src/entities/publishers.entity.ts
+++ server/src/entities/publishers.entity.ts
@@ -2,7 +2,7 @@ import { Field, ObjectType } from "type-graphql";
 import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 @ObjectType()
 @Entity()
-export class BookFavoritesEntity extends BaseEntity {
+export class PublishersEntity extends BaseEntity {
   @Field()
   @PrimaryGeneratedColumn("uuid")
   publisherid: string;
