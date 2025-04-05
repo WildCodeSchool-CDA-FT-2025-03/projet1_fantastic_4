ed6af1976e4df764cb9a1913da29779e537cc944
Maximilien <maximilien.philippe@protonmail.com>
2025-04-05 17:24:55 +0200

    feat(020-us): add pegiesbr service to data services

--- server/src/services/datas.service.ts
+++ server/src/services/datas.service.ts
@@ -1,11 +1,12 @@
 import { GamesEntity } from "../entities/games/games.entity";
 import { CategoriesEntity } from "../entities/categories.entity";
 import { DataSource } from "typeorm";
+import { GamesPegiEsbr } from "../entities/games/pegiesbr.entity";
 
 const dataSource = new DataSource({
   type: "sqlite",
   database: "./db.sqlite",
-  entities: [CategoriesEntity, GamesEntity],
+  entities: [CategoriesEntity, GamesEntity, GamesPegiEsbr],
   synchronize: true, //pas en prod
 });
 
