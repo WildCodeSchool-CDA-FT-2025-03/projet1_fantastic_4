34b758219abf5709b8dea6f7a0e831d1a1c6e788
Emilie <150048527+EmiLy-Ly-san@users.noreply.github.com>
2025-04-08 15:22:12 +0200

    feat(043): types change in fakeDatas for the carousel for API datas

--- client/src/components/Carousel/Carousel.tsx
+++ client/src/components/Carousel/Carousel.tsx
@@ -12,8 +12,9 @@ type CarouselProps = {
 type Data = {
   id: number;
   title: string;
-  category_name: string;
+  categoryId: string;
   genre: string;
+  category: { id: number; name: string };
 };
 
 const SLIDEWITHPIXELS = 296 + 9; //we add the gap
@@ -112,7 +113,7 @@ export default function Carousel({ datas, title_carousel }: CarouselProps) {
               <CardMedia
                 key={item.id}
                 title={item.title}
-                category_name={item.category_name}
+                category_name={item.category.name}
                 genre={item.genre}
                 id={item.id}
                 ref={index === datas.length - 1 ? lastItemRef : undefined}
