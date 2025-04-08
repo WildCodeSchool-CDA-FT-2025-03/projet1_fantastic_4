afb7c28a88a6a7aea113ee1adb6e338e7125acc4
Emilie <150048527+EmiLy-Ly-san@users.noreply.github.com>
2025-04-08 17:08:49 +0200

    feat(001): icons burger menu conditionnal return done

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,13 +1,27 @@
 import Navbar from "./Navbar/Navbar";
 import "./Header.css";
+import { useState } from "react";
 
 const Header = () => {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
   return (
     <>
       <header>
         <div className="logo-header"></div>
         <img src="/media-hub-icon.webp"></img>
         <Navbar />
+        <img
+          className="burgerMenu"
+          src={isMenuOpen ? "public/close.svg" : "public/burgerMenu.svg"}
+          alt=""
+          onClick={() => setMenuOpen(!isMenuOpen)}
+        />
+        {isMenuOpen ? (
+          <nav className="navbar-mobile">
+            <Navbar />
+          </nav>
+        ) : null}
       </header>
     </>
   );
