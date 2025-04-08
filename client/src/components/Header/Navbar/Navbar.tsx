618a21b4e3e8e9adc5266c8546e0a069617c6b3d
Emilie <150048527+EmiLy-Ly-san@users.noreply.github.com>
2025-04-08 17:39:28 +0200

    feat(001): map on real data categories in navbar

--- client/src/components/Header/Navbar/Navbar.tsx
+++ client/src/components/Header/Navbar/Navbar.tsx
@@ -1,17 +1,18 @@
 import { NavLink } from "react-router";
 import "./Navbar.css";
 
-const categoriesData = [
-  { id: 1, name: "movies" },
-  { id: 1, name: "musics" },
-  { id: 1, name: "games" },
-  { id: 1, name: "books" },
-];
+import { useQuery } from "@apollo/client";
+import { GET_ALL_CATEGORIES } from "@/schemas/categories.schema";
+
+type GetAllCategoriesType = {
+  getAllCategories: { id: number; name: string }[];
+};
 
 const Navbar = () => {
+  const { data } = useQuery<GetAllCategoriesType>(GET_ALL_CATEGORIES);
   return (
     <ul className="navlinks">
-      {categoriesData.map((category) => (
+      {data?.getAllCategories.map((category) => (
         <li>
           <NavLink
             to={`/${category.name}`}
