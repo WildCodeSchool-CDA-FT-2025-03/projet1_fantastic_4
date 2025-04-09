90cee91e7fb8f11ef1c769623ca05609304dcdca
Florian Sebal <fsebal91@gmail.com>
2025-04-09 21:10:16 +0200

    feat(042-ft): update books entity relations

--- server/src/entities/books.entity.ts
+++ server/src/entities/books.entity.ts
@@ -1,5 +1,13 @@
 import { Field, ObjectType } from "type-graphql";
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  JoinColumn,
+  ManyToOne,
+} from "typeorm";
+import { CategoriesEntity } from "./categories.entity";
 
 @ObjectType()
 @Entity("book")
@@ -15,4 +23,18 @@ export class BooksEntity extends BaseEntity {
   @Field()
   @Column()
   title: string;
+
+  //  Foreign Key CategoriesEntity
+  @Column({ default: 1 }) // Force ID 1
+  @Field()
+  categoryId: number = 3;
+
+  // CategoriesEntity relation
+  @ManyToOne(() => CategoriesEntity, (category) => category.books, {
+    eager: true,
+    onDelete: "CASCADE",
+  })
+  @JoinColumn({ name: "categoryId" }) // Associates categoryId to CategoriesEntity
+  @Field(() => CategoriesEntity)
+  category: CategoriesEntity;
 }
