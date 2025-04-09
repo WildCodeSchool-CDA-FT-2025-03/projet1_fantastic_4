c98967bde8edbe63c7688c3b5997279fce4f2954
Maximilien <maximilien.philippe@protonmail.com>
2025-04-09 16:27:40 +0200

    feat(057-us): add carousel game in page game

--- client/src/pages/games/GamesPage.tsx
+++ client/src/pages/games/GamesPage.tsx
@@ -1,9 +1,11 @@
+import CarouselNewInGame from "@/components/CarouselNewInGame";
 import "./GamesPage.css";
 
 const GamesPage = () => {
   return (
     <>
       <h2>Games Page</h2>
+      <CarouselNewInGame />
     </>
   );
 };
