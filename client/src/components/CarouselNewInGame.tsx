81237b97cf75a396735af70dd4f121b78f38f65f
Maximilien <maximilien.philippe@protonmail.com>
2025-04-09 16:24:50 +0200

    feat(057-us): add new carousel for game

---
+++ client/src/components/CarouselNewInGame.tsx
@@ -0,0 +1,44 @@
+import { useQuery } from "@apollo/client";
+import Carousel from "./Carousel/Carousel";
+import { GET_GAMES_CAROUSEL } from "@/schemas/games.schema";
+import { Data } from "./Carousel/Carousel";
+import { Category } from "@/types/category.type";
+
+type GetGameNewInType = {
+  getGames: {
+    slug: string;
+    coverUrl: string;
+    category: { name: string };
+    title: string;
+  }[];
+};
+
+export default function CarouselNewInGame() {
+  const { loading, error, data } = useQuery<GetGameNewInType>(
+    GET_GAMES_CAROUSEL,
+    {
+      variables: { limit: 10, order: "releaseDate", dir: "desc" },
+    },
+  );
+
+  const gameNewIn = data?.getGames.map((g) => {
+    return {
+      id: g.slug, // useless
+      title: g.title,
+      genre: g.category.name,
+      category: { id: 0, name: Category.Games }, // id: useless
+      categoryId: "0", // useless
+    } as Data;
+  });
+
+  if (loading) return <p>Loading in progress...</p>;
+  if (error) return <p>There might be an error</p>;
+
+  return (
+    <>
+      {gameNewIn && (
+        <Carousel datas={gameNewIn} title_carousel="New In Games" />
+      )}
+    </>
+  );
+}
