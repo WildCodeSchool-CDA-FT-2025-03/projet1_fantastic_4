599870fbaeca30f2a27170d8d3a8cc75654ae8ce
Maximilien <maximilien.philippe@protonmail.com>
2025-04-10 11:18:22 +0200

    feat(057-us): add title to cover

--- client/src/components/Carousel/CardMedia.tsx
+++ client/src/components/Carousel/CardMedia.tsx
@@ -25,13 +25,14 @@ function CardMedia({
         onClick={() => window.scrollTo({ top: 0, left: 0, behavior: "smooth" })}
       >
         <section className={`card-content ${category_name}`}>
-          {coverUrl && <img className="card-cover" src={coverUrl} />}
+          {(coverUrl && (
+            <img title={title} className="card-cover" src={coverUrl} />
+          )) || <h2>{title}</h2>}
           <img
             className="category-icon"
             src={`/public/${category_name}-icon.svg`}
             alt={`${category_name} icon`}
           />
-          {!coverUrl && <h2>{title}</h2>}
         </section>
       </Link>
       <section className="card-footer">
