793e4c42fbfb249425dbb4caba03cedc0b32c01e
Florian Sebal <fsebal91@gmail.com>
2025-04-11 12:55:34 +0200

    feat(071-ft): add book section

--- client/src/pages/books/BooksPage.tsx
+++ client/src/pages/books/BooksPage.tsx
@@ -1,11 +1,16 @@
 import CarouselNewInBooks from "@/components/CarouselNewInBooks";
 import "./BooksPage.css";
 import CarouselRecoBooks from "@/components/CarouselRecoBooks";
+import MediaPanel from "@/components/Panel/MediaPanel";
 
 const BooksPage = () => {
   return (
     <>
-      <h2>Books Page</h2>
+      <div className="books-page" id="top">
+        {" "}
+      </div>
+      <MediaPanel class="books-page-title" title="Books section" />
+      <section className="books-container"></section>
       <CarouselNewInBooks />
       <CarouselRecoBooks />
     </>
